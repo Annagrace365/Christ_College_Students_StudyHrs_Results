e4b85a854faef0df75dd574103d16a9f9b30302e
Anna Grace Raju <148966297+Annagrace365@users.noreply.github.com>
2026-09-17 09:25:53 +0530

    Add Streamlit app for predicting student pass/fail

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit
+import joblib
+
+model=joblib.load("logistic_regression_studyhours_model.pkl")
+st.title("Student Pass/Fail based on Study Hours")
+hours=st.number_input("Enter Study Hours:", min_value=0.0 ,max_value=15.0, value=5.0)
+if st.button("Predict"):
+  prediction=model.predict([[hours]])
+  if prediction[0]==1:
+    st.success("Pass")
+  else:
+    st.error("Fail")

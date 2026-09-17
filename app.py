3b43ff5096849643908c5f6a166b75f2f2608cba
Anna Grace Raju <148966297+Annagrace365@users.noreply.github.com>
2026-09-17 09:33:55 +0530

    Update prediction model to include attendance input

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ import joblib
 model=joblib.load("logistic_regression_studyhours_model.pkl")
 st.title("Student Pass/Fail based on Study Hours")
 hours=st.number_input("Enter Study Hours:", min_value=0.0 ,max_value=15.0, value=5.0)
+attendance=st.number_input("Enter Attendance:", min_value=0 ,max_value=100, value=90)
 if st.button("Predict"):
-  prediction=model.predict([[hours]])
+  prediction=model.predict([[hours,attendance]])
   if prediction[0]==1:
     st.success("Pass")
   else:

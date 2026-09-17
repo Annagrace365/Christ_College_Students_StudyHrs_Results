84d59581d131b89c193951162f4a4fe8e1ac76c5
Anna Grace Raju <148966297+Annagrace365@users.noreply.github.com>
2026-09-17 11:02:21 +0530

    Add attendance input to prediction model

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ import joblib
 model=joblib.load("logistic_regression_studyhours_model.pkl")
 st.title("Student Pass/Fail based on Study Hours")
 hours=st.number_input("Enter Study Hours:", min_value=0.0 ,max_value=15.0, value=5.0)
+attendance=st.number_input("Enter Attendance:", min_value=0 ,max_value=100, value=75)
+
 if st.button("Predict"):
-  prediction=model.predict([[hours]])
-  probability = model.predict_proba([[hours]])
+  prediction=model.predict([[hours,attendance]])
+  probability = model.predict_proba([[hours,attendance]])
   pass_prob=probability[0][1]*100
   fail_prob=probability[0][0]*100
   if prediction[0]==1:
